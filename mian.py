c7dd5b0f74043acd0babc5ca33e67a46b9b31067
Sikandarhayatmarkhal <sikandar1071@gmail.com>
2024-08-13 18:09:59 +0500

    intial commits
    this is just a simple joke generator programme

---
+++ mian.py
@@ -0,0 +1,9 @@
+def main():
+    user_want=input("what you want : joke ").lower()
+    if user_want=="joke":
+        print("Here is a joke for you! Panaversity GPT - Sophia is heading out to the grocery store. A programmer tells her: get a liter of milk, and if they have eggs, get 12. Sophia returns with 13 liters of milk. The programmer asks why and Sophia replies: 'because they had eggs'")
+    else:
+        print("sorry i can only tell the jokes")
+# print(__name__)
+if __name__=='__main__':
+    main()
